2914cd9a3cb84526591fb113b45a8da14c30b663
Antoine Cheron <cheron.antoine@gmail.com>
2020-06-03 14:44:10 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3571074bcc6e7040820e.js"
+    "url": "webpack-runtime-1bee155b338ff2fa0ed2.js"
   },
   {
     "url": "framework-972833c791692296cdd7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f81c186298428aad6063709f6c85e6e5"
+    "revision": "d710240ca809441a1b13460d4f29338a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-3dab3f8a67c1189caac1.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c955451df6f276503e82ecd5a4e8c02d"
+    "revision": "5047f6c5e9744eeb836fb548e151a74d"
   },
   {
     "url": "manifest.webmanifest",
